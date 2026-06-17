022bbf9a56a054faffc054aab10a328370bf0e6d
Alex0ar <arnautalexandru2@gmail.com>
2026-06-17 23:35:49 +0300

    initial commit

---
+++ Json-database-with-java/src/main/java/org/example/server/JsonStorage.java
@@ -0,0 +1,13 @@
+package org.example.server;
+
+import com.google.gson.JsonArray;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JsonStorage {
+    private static final Path FILE = Paths.get("data.json");
+    private JsonArray data;
+    //de adaugat toate metodele functionalitati si cea de initializare a fisierului
+
+}
---
+++ Json-database-with-java/src/main/java/org/example/server/Main.java
@@ -0,0 +1,15 @@
+package org.example.server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
