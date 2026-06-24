9f6551c7d9aaa2b9d72406f4fa4b74550315e36e
Alex0ar <arnautalexandru2@gmail.com>
2026-06-24 20:17:19 +0300

    clear project structure

--- Json-database-with-java/src/main/java/org/example/client/Main.java
+++ Json-database-with-java/src/main/java/org/example/client/Main.java
@@ -1,7 +1,28 @@
 package org.example.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
+    private static final String address = "127.0.0.1";
+    private static final int port = 23456;
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try(
+                Socket socket = new Socket(InetAddress.getByName(address), port);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        ) {
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
--- Json-database-with-java/src/main/java/org/example/server/Main.java
+++ Json-database-with-java/src/main/java/org/example/server/Main.java
@@ -1,13 +1,41 @@
 package org.example.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
+    private static final String address = "127.0.0.1";
+    private static final int port = 23456;
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.readClientRequests();
+        // **-- STAGE 1 --**
+//        Controller controller = new Controller();
+//        controller.readClientRequests();
+
+        // ** -- STAGE 2 --**
+        try (
+                ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(address));
+        ) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                ) {
+                    output.writeUTF("Hello World");
+                    output.flush();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
