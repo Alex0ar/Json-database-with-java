a964a557547cf46deb5818f4bb6a0f4cf8de356a
Alex0ar <arnautalexandru2@gmail.com>
2026-06-26 08:00:29 +0300

    stage 3 complete

--- Json-database-with-java/src/main/java/org/example/server/Controller.java
+++ Json-database-with-java/src/main/java/org/example/server/Controller.java
@@ -21,22 +21,26 @@ public class Controller {
                 ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(address));
         ) {
             System.out.println("Server started!");
-            try (
-                    Socket socket = serverSocket.accept();
-                    DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-                    DataInputStream input = new DataInputStream(socket.getInputStream());
-            ) {
-                String msg = input.readUTF();
-                String[] msgArray = msg.split(" ", 3);
-                String response = switch (msgArray[0]) {
-                    case "get" -> jsonStorage.get(Integer.parseInt(msgArray[1]));
-                    case "set" -> jsonStorage.set(Integer.parseInt(msgArray[1]), msgArray[2]);
-                    case "delete" -> jsonStorage.remove(Integer.parseInt(msgArray[1]));
-                    default -> throw new IllegalStateException("Unexpected value: " + msgArray[0]);
-                };
-
-                output.writeUTF(response);
-
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                ) {
+                    String msg = input.readUTF();
+                    String[] msgArray = msg.split(" ", 3);
+                    String response = switch (msgArray[0]) {
+                        case "get" -> jsonStorage.get(Integer.parseInt(msgArray[1]));
+                        case "set" -> jsonStorage.set(Integer.parseInt(msgArray[1]), msgArray[2]);
+                        case "delete" -> jsonStorage.remove(Integer.parseInt(msgArray[1]));
+                        case "exit" -> "OK";
+                        default -> throw new IllegalStateException("Unexpected value: " + msgArray[0]);
+                    };
+                    output.writeUTF(response);
+                    if (msgArray[0].equals("exit")) {
+                        break;
+                    }
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- Json-database-with-java/src/main/java/org/example/server/Main.java
+++ Json-database-with-java/src/main/java/org/example/server/Main.java
@@ -11,33 +11,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-    private static final String address = "127.0.0.1";
-    private static final int port = 23456;
+
     public static void main(String[] args) {
-        // **-- STAGE 1 --**
-//        Controller controller = new Controller();
-//        controller.readClientRequests();
-
-        // ** -- STAGE 2 --**
-        try (
-                ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(address));
-        ) {
-            System.out.println("Server started!");
-            try (
-                    Socket socket = serverSocket.accept();
-                    DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-                    DataInputStream input = new DataInputStream(socket.getInputStream());
-            ) {
-                String msg = input.readUTF();
-                String[] parts = msg.split(" ");
-                System.out.println("Received: " + msg);
-                output.writeUTF("A record # " + parts[parts.length - 1] + " was sent!");
-                System.out.println("Sent: A record # " + parts[parts.length - 1] + " was sent!");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        Controller controller = new Controller();
+        controller.startServer();
     }
 }
