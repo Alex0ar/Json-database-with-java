f68460ef068ceccc53bff1547d6d16e472fee00a
Alex0ar <arnautalexandru2@gmail.com>
2026-07-01 08:44:40 +0300

    JsonStorage class edit in progress

--- Json-database-with-java/src/main/java/org/example/server/JsonStorage.java
+++ Json-database-with-java/src/main/java/org/example/server/JsonStorage.java
@@ -1,61 +1,69 @@
-package org.example.server;
+package server;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
-
 public class JsonStorage {
-    private static final Path FILE = Paths.get("data.json");
-    private JsonArray data;
+    private static final Path FILE = Paths.get("../data.json");
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private JsonObject data;
 
-    public void load() throws IOException {
-        String content = Files.readString(FILE);
-        data = JsonParser.parseString(content).getAsJsonArray();
+    public JsonStorage() {
+        try (FileReader fileReader = new FileReader(FILE.toFile())) {
+            JsonElement element = JsonParser.parseReader(fileReader).getAsJsonObject();
+            if (element.isJsonObject()) {
+                data = new JsonObject();
+            } else {
+                data = element.getAsJsonObject();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+//    public void load() throws IOException {
+//        String content = Files.readString(FILE);
+//        data = JsonParser.parseString(content).getAsJsonArray();
+//    }
+
     public void save() throws IOException {
-        Files.writeString(FILE, data.toString());
+        try (FileWriter fileWriter = new FileWriter(FILE.toFile())) {
+            gson.toJson(data, fileWriter);
+        }
     }
 
     public String get(int i) {
-        if (i < 0 || i >= data.size() || data.get(i).getAsString() == "") {
+        if (i < 1 || i > data.size() || data.get(i - 1).getAsString() == "") {
             return "ERROR";
         }
-        return data.get(i).getAsString();
+        return data.get(i - 1).getAsString();
     }
 
     public String set(int i, String value) {
-        if (i < 0 || i >= data.size()) {
+        if (i < 1 || i > data.size()) {
             return "ERROR";
         } else {
-            data.set(i, new JsonPrimitive(value));
+            data.set(i - 1, new JsonPrimitive(value));
             return "OK";
         }
     }
 
     public String remove(int i) {
-        if (i < 0 || i >= data.size()) {
+        if (i < 1 || i > data.size()) {
             return "ERROR";
         } else {
-            data.set(i, new JsonPrimitive(""));
+            data.set(i - 1, new JsonPrimitive(""));
             return "OK";
         }
-    }
-
-    //INIT
-    public void initialize() throws IOException {
-        data = new JsonArray();
-        for (int i = 0; i < 1000; i++) {
-            data.add("");
-        }
-        Files.writeString(FILE, data.toString());
     }
 
 }
